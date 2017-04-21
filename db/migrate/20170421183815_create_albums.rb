58bf119a90200d529632166a54d0ca5a01567d0f
dalvarez131 <dalvarez131@gmail.com>
2017-04-21 14:56:52 -0500

    Version Inicial 5

--- db/migrate/20170421183815_create_albums.rb
+++ db/migrate/20170421183815_create_albums.rb
@@ -2,7 +2,7 @@ class CreateAlbums < ActiveRecord::Migration[5.0]
   def change
     create_table :albums do |t|
       t.string :name
-      t.string :imagen_url
+      t.string :image_url
       t.datetime :released_at
       t.integer :artist_id
 
